2272562fa531ddff04f2ffff3cc9b720f7fbb001
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-02-25 19:09:07 -0300

    fix: reorder imports in layout.tsx for better organization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { ThemeProvider } from "@/components/theme/theme-provider";
 import { GoogleAnalytics } from "@/components/GoogleAnalytics";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 // import { Footer } from "@/components/footer/Footer";
 
